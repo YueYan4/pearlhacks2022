989a25eaf6196022ced2a192b3a55598398df5b3
YueYan4 <yueyan@email.unc.edu>
2022-02-19 15:13:36 -0500

    Finished but can't get mic input to work

--- Speech.py
+++ Speech.py
@@ -1,28 +1,37 @@
+from distutils.command.check import SilentReporter
 import speech_recognition as sr
+import time
 
 r = sr.Recognizer()
 r.energy_threshold = 300
 mic = sr.Microphone()
-
+    
 #function to get mic input and write it to a txt file
 def recognize_speech_from_mic(r, mic):
+    print("Start talking")
     with mic as source:
         r.adjust_for_ambient_noise(source)
-        audio = r.record(source)
+        audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
             diary_file = open('diary.txt', 'a')
             diary_file.write(text)
             diary_file.close()
+            print(text)
             return diary_file
         except Exception:
-            return "Error"
+            print("Sorry, I didn't get that")
    
 #function to read the txt file 
-def read_file(diary_file, chunk_size=5242880):
+def read_file(diary_file):
     with open(diary_file, 'rb') as _file:
         while True:
-            data = _file.read(chunk_size)
+            data = _file.read()
             if not data:
                 break
             yield data
+  
+  
+time.sleep(1)          
+while 1:          
+    recognize_speech_from_mic(r, mic)
