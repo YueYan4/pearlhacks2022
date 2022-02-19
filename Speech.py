d22bdea49b9d71480677f121e83ae5f4378857cb
YueYan4 <yueyan@email.unc.edu>
2022-02-19 13:06:03 -0500

    Updated

--- Speech.py
+++ Speech.py
@@ -1,18 +1,22 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
+r.energy_threshold = 300
 mic = sr.Microphone()
 
 
 def recognize_speech_from_mic(r, mic):
     with mic as source:
         r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-        text = r.recognize_google(audio, True, language = 'en')
-        diary_file = open('diary.txt', 'a')
-        diary_file.write(text)
-        diary_file.close()
-        return diary_file
+        audio = r.record(source)
+        try:
+            text = r.recognize_google(audio)
+            diary_file = open('diary.txt', 'a')
+            diary_file.write(text)
+            diary_file.close()
+            return diary_file
+        except Exception:
+            return "Error"
     
 def read_file(diary_file, chunk_size=5242880):
     with open(diary_file, 'rb') as _file:
